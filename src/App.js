4d529b72d6fa9f51add3aeb4145afc3321028481
engkuazrul <engkuazrull@gmail.com>
2021-05-11 22:17:38 +0800

    init web

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import './App.css'; 
+
+const app = new Clarifai.App({
+  apiKey: '33ae7ac179b94a39b5d36b4a6778a499'
+ }); 
+
+const particlesOptions = {
+   particles: {
+     line_linked: {
+       shadow: {
+         enable: true, 
+         color: "#3CA9D1",
+         blur: 5
+       }
+     }
+   }
+}
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onSubmit = () => {
+    console.log('click');
+    app.models.predict("eeed0b6733a644cea07cf4c60f87ebb7", "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZmFjZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80").then(
+      function(response){
+        console.log(response);
+      },
+      function(err){
+        console.log(err);
+      }
+    );
+  }
+
+  render () {
+    return (
+      <div className="App">
+          <Particles className='particles'
+            params={particlesOptions}
+          />
+          <Navigation />
+          <Logo />
+          <Rank /> 
+          <ImageLinkForm onInputChange={this. onInputChange} onSubmit={this.onSubmit}/>  
+          <FaceRecognition />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FaceRecognition = () => {
+    return (
+        <div className='center'>
+            <img alt='' src={'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZmFjZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=300&q=80'}/>
+        </div>
+    );
+}
+
+export default FaceRecognition ;
